4e02b573d119467f47773838fe59e4644fbf6058
doi-shuhei <doi@nagisalabs.org>
2016-09-09 13:13:03 +0900

    modify GoogleTrendsPersingUtil to read in progress month

--- src/main/java/com/heroku/myapp/commons/exceptions/MessageNotSetException.java
+++
@@ -1,5 +0,0 @@
-package com.heroku.myapp.commons.exceptions;
-
-public class MessageNotSetException extends RuntimeException {
-
-}
--- src/main/java/com/heroku/myapp/commons/util/content/GoogleTrendsParsingUtil.java
+++ src/main/java/com/heroku/myapp/commons/util/content/GoogleTrendsParsingUtil.java
@@ -9,20 +9,22 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class GoogleTrendsParsingUtil {
-    
+
     private final Map<String, List<Map<String, String>>> body;
     private final Map<String, List<Map<String, String>>> bodyNotContainingScale
             = new LinkedHashMap<>();
-    
+
     private final String scale;
     private final String threshold;
     private final Map<String, String> fromMap = new LinkedHashMap<>();
+    private final Map<String, Integer> progressMap = new LinkedHashMap<>();
     private final int size;
-    
+
     public GoogleTrendsParsingUtil(Map<String, List<Map<String, String>>> body, String scale, String threshold) {
         this.body = body;
         this.scale = scale;
         this.threshold = threshold;
+        removeProgress(body);
         body.entrySet().forEach((entry) -> {
             String name = entry.getKey();
             List<Map<String, String>> value = entry.getValue();
@@ -33,7 +35,7 @@ public class GoogleTrendsParsingUtil {
         });
         this.size = body.get(scale).size();
     }
-    
+
     public void mainLogic() {
         if (scaleIsValid()) {
             createSuccessResults();
@@ -41,11 +43,11 @@ public class GoogleTrendsParsingUtil {
             createFailedResults();
         }
     }
-    
+
     public boolean scaleIsValid() {
         return max(scale) == 100;
     }
-    
+
     private IntStream stream(String name) {
         return body.get(name).stream()
                 .map((map) -> map.values().iterator().next())
@@ -60,51 +62,51 @@ public class GoogleTrendsParsingUtil {
                 .map((map) -> map.values().iterator().next())
                 .mapToInt(Integer::parseInt);
     }
-    
+
     private int max(String name) {
         return stream(name).max().orElse(0);
     }
-    
+
     private long count(String name) {
         return stream(name).count();
     }
-    
+
     private double average(String name) {
         return streamFrom(name).average().orElse(0.0);
     }
-    
+
     private int sum(String name) {
         return stream(name).sum();
     }
-    
+
     private Set<String> overNames() {
         int scaleMax = max(scale);
         return bodyNotContainingScale.keySet().stream()
                 .filter((name) -> {
-                    return max(name) > scaleMax;
+                    return Math.max(max(name), progressMap.get(name)) > scaleMax;
                 })
                 .collect(Collectors.toSet());
     }
-    
+
     private String getFromMonth(List<Map<String, String>> list) {
         return list.stream().filter((map)
                 -> map.values().iterator().next().compareTo(threshold) >= 0)
                 .map((map) -> map.keySet().iterator().next())
                 .findFirst().orElse("9999");
     }
-    
+
     public List<Map<String, Object>> createSuccessResults() {
         return bodyNotContainingScale.keySet().stream()
                 .map((name) -> createSuccessResult(name))
                 .collect(Collectors.toList());
     }
-    
+
     public List<Map<String, Object>> createFailedResults() {
         return overNames().stream()
                 .map((name) -> createFailedResult(name))
                 .collect(Collectors.toList());
     }
-    
+
     private Map<String, Object> createSuccessResult(String name) {
         Map<String, Object> result = new LinkedHashMap<>();
         result.put("name", name);
@@ -120,7 +122,7 @@ public class GoogleTrendsParsingUtil {
         result.put("date", new Date());
         return result;
     }
-    
+
     private Map<String, Object> createFailedResult(String name) {
         Map<String, Object> result = new LinkedHashMap<>();
         result.put("name", name);
@@ -128,5 +130,20 @@ public class GoogleTrendsParsingUtil {
         result.put("status", "failed");
         result.put("date", new Date());
         return result;
+    }
+
+    private void removeProgress(Map<String, List<Map<String, String>>> body) {
+        Set<String> keys = body.keySet();
+        keys.stream().forEach((key) -> {
+            List<Map<String, String>> value = body.get(key);
+            for (int i = 0; i < value.size(); i++) {
+                String get = value.get(i).get("progress");
+                if (get != null) {
+                    progressMap.put(key, Integer.parseInt(get));
+                    value.remove(i);
+                    break;
+                }
+            }
+        });
     }
 }
