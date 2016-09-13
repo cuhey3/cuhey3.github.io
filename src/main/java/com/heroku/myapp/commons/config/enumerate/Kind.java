980cc0219d6124f5e2efdd6bd53fed3aa1b82137
doi-shuhei <doi@nagisalabs.org>
2016-09-13 16:35:42 +0900

    prototyping Kind.recentchanges_of_seiyu

--- src/main/java/com/heroku/myapp/commons/config/enumerate/Kind.java
+++ src/main/java/com/heroku/myapp/commons/config/enumerate/Kind.java
@@ -36,7 +36,8 @@ public enum Kind {
     google_trends,
     seiyu_multi_lang,
     categories_related_seiyu,
-    pages_related_seiyu;
+    pages_related_seiyu,
+    recentchanges_of_seiyu;
 
     private String message;
     private boolean useDevelop = false;
