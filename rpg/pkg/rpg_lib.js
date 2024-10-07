598a83dc12d1c3e370d7c8b40f326917aea664bb
cuhey3 <uotaneet@gmail.com>
2024-10-08 01:04:41 +0900

    update prg page

--- rpg/pkg/rpg_lib.js
+++ rpg/pkg/rpg_lib.js
@@ -835,8 +835,8 @@ function __wbg_get_imports() {
         const ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper781 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 91, __wbg_adapter_28);
+    imports.wbg.__wbindgen_closure_wrapper791 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 95, __wbg_adapter_28);
         return addHeapObject(ret);
     };
 
