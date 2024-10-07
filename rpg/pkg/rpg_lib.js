1852d43a0d23b70ec9a2decb94647594b803e4ba
cuhey3 <uotaneet@gmail.com>
2024-10-08 01:06:59 +0900

    update prg page

--- rpg/pkg/rpg_lib.js
+++ rpg/pkg/rpg_lib.js
@@ -890,7 +890,7 @@ async function __wbg_init(module_or_path) {
     console.warn('using deprecated parameters for the initialization function; pass a single object instead')
 
     if (typeof module_or_path === 'undefined') {
-        module_or_path = new URL('rpg_lib_bg.wasm', import.meta.url);
+        module_or_path = new URL('rpg_lib_bg.wasm?t=202410080105', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
