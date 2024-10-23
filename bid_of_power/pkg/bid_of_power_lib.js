25b04f6d8b2e677af877dc7b5c6569351f0d8eef
cuhey3 <uotaneet@gmail.com>
2024-10-23 20:46:11 +0900

    fix bug

--- bid_of_power/pkg/bid_of_power_lib.js
+++ bid_of_power/pkg/bid_of_power_lib.js
@@ -957,7 +957,7 @@ async function __wbg_init(module_or_path) {
     console.warn('using deprecated parameters for the initialization function; pass a single object instead')
 
     if (typeof module_or_path === 'undefined') {
-        module_or_path = new URL('bid_of_power_lib_bg.wasm?t=202410232000', import.meta.url);
+        module_or_path = new URL('bid_of_power_lib_bg.wasm?t=202410232100', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
